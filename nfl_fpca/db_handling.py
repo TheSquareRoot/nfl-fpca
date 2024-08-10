061cdb0dab4d158f7766e689b4d7b27c2f486290
TheSquareRoot <victor.bourgin1@gmail.com>
2024-08-10 13:27:12 +0200

    Added basic error handling to db_handling.py

--- nfl_fpca/db_handling.py
+++ nfl_fpca/db_handling.py
@@ -41,28 +41,31 @@ def add_players(player_list):
     for player in player_list:
         # PlayerInfo table
         logger.debug(f"[{player.pid}] - Adding player...")
-        db_model.PlayerInfo.create(pid=player.pid,
-                                   first_name=player.first_name,
-                                   last_name=player.last_name,
-                                   position=player.position,
-                                   height=player.height,
-                                   weight=player.weight,
-                                   start_year=player.start_year,
-                                   start_age=player.start_age,
-                                   career_length=player.career_length,
-                                   retired=player.retired,
-                                   dash=player.dash,
-                                   bench=player.bench,
-                                   broad=player.broad,
-                                   shuttle=player.shuttle,
-                                   cone=player.cone,
-                                   vertical=player.vertical, )
+        try:
+            db_model.PlayerInfo.create(pid=player.pid,
+                                       first_name=player.first_name,
+                                       last_name=player.last_name,
+                                       position=player.position,
+                                       height=player.height,
+                                       weight=player.weight,
+                                       start_year=player.start_year,
+                                       start_age=player.start_age,
+                                       career_length=player.career_length,
+                                       retired=player.retired,
+                                       dash=player.dash,
+                                       bench=player.bench,
+                                       broad=player.broad,
+                                       shuttle=player.shuttle,
+                                       cone=player.cone,
+                                       vertical=player.vertical, )
 
-        # SeasonStats table
-        for year, val in player.stats.items():
-            db_model.SeasonStats.create(pid=player.pid,
-                                        year=year,
-                                        games_played=val['gp'],
-                                        approx_value=val['av'], )
+            # SeasonStats table
+            for year, val in player.stats.items():
+                db_model.SeasonStats.create(pid=player.pid,
+                                            year=year,
+                                            games_played=val['gp'],
+                                            approx_value=val['av'], )
+        except Exception as e:
+            logger.exception(e)
 
     logger.info(f"{len(player_list)} players added to database!")
