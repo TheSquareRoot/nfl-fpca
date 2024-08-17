c5b648f256462eb881873e51b9b402574578badd
TheSquareRoot <victor.bourgin1@gmail.com>
2024-08-17 12:49:27 +0200

    Simplified the scrape_player_page function by moving the extraction into the relevant scraping functions.

--- nfl_fpca/scraping/player_scrapper.py
+++ nfl_fpca/scraping/player_scrapper.py
@@ -16,85 +16,110 @@ from .utils import (find_table,
 logger = setup_logging(__name__, 'logs/scraping.log')
 
 
-def scrape_player_header(header, player):
-    # Compile regular expressions
-    position_re = re.compile(r":\s([\w-]+)")
-    height_re = re.compile(r"(\d{3})cm")
-    weight_re = re.compile(r"(\d{2,3})kg")
+def scrape_player_header(soup, player):
+    # Extract the header from the page
+    header = soup.find('div', attrs={'id': 'meta'})
 
-    # Name
-    name = header.h1.span.text
+    if header:
+        # Compile regular expressions
+        position_re = re.compile(r":\s([\w-]+)")
+        height_re = re.compile(r"(\d{3})cm")
+        weight_re = re.compile(r"(\d{2,3})kg")
 
-    # Position
-    pos_txt = header.find(string="Position")
-    if pos_txt is not None:
-        pos = position_re.search(pos_txt.parent.parent.text).group(1)
-        pos_group = get_position_group(pos)
+        # Name
+        name = header.h1.span.text
+
+        # Position
+        pos_txt = header.find(string="Position")
+        if pos_txt is not None:
+            pos = position_re.search(pos_txt.parent.parent.text).group(1)
+            pos_group = get_position_group(pos)
+        else:
+            logger.debug(f"[{player.pid}] - No position found")
+            pos = 'N/A'
+            pos_group = 'N/A'
+
+        # Physicals
+        phys = header.find_all(string=height_re)
+        if phys:
+            height = int(height_re.search(phys[0].text).group(1))
+            weight = int(weight_re.search(phys[0].text).group(1))
+        else:
+            logger.info(f"[{player.pid}] - No physicals found")
+            height = 0
+            weight = 0
+
+        # Update player info
+        player.set_player_info(name, pos, pos_group, height, weight)
+
     else:
-        logger.debug(f"[{player.pid}] - No position found")
-        pos = 'N/A'
-        pos_group = 'N/A'
+        logger.debug(f"[{player.pid}] - No player header")
 
-    # Physicals
-    phys = header.find_all(string=height_re)
-    if phys:
-        height = int(height_re.search(phys[0].text).group(1))
-        weight = int(weight_re.search(phys[0].text).group(1))
+
+def scrape_career_table(soup, player):
+    # Extract career table from page
+    table = get_career_table(soup)
+
+    if table:
+        logger.debug(f"[{player.pid}] - Career info found in {table['id']}")
+        stats = dict()
+
+        # Extract the table data lines
+        lines = table.tbody.find_all('tr', attrs={'class': 'full_table'})
+
+        # Get approximate value and games played for each year
+        for line in lines:
+            year = int(line['id'].split('.')[1])
+            gp = int(line.find('td', attrs={'data-stat': 'g'}).text or 0)
+            gs = int(line.find('td', attrs={'data-stat': 'gs'}).text or 0)
+            av = int(line.find('td', attrs={'data-stat': 'av'}).text or 0)
+
+            stats[year] = {'gp': gp, 'gs': gs, 'av': av}
+
+        # Get other information
+        start_year = int(lines[0]['id'].split('.')[1])
+        last_year = int(lines[-1]['id'].split('.')[1])
+        start_age = int(lines[0].find('td', attrs={'data-stat': 'age'}).text)
+
+        # Update player info
+        player.set_career_info(start_year, start_age, last_year, stats)
+
     else:
-        logger.info(f"[{player.pid}] - No physicals found")
-        height = 0
-        weight = 0
-
-    # Update player info
-    player.set_player_info(name, pos, pos_group, height, weight)
+        logger.debug(f"[{player.pid}] - No career table")
 
 
-def scrape_career_table(table, player):
-    lines = table.tbody.find_all('tr', attrs={'class': 'full_table'})
-
-    stats = dict()
-
-    # Get approximate value and games played for each year
-    for line in lines:
-        year = int(line['id'].split('.')[1])
-        gp = int(line.find('td', attrs={'data-stat': 'g'}).text or 0)
-        gs = int(line.find('td', attrs={'data-stat': 'gs'}).text or 0)
-        av = int(line.find('td', attrs={'data-stat': 'av'}).text or 0)
-
-        stats[year] = {'gp': gp, 'gs': gs, 'av': av}
-
-    # Get other information
-    start_year = int(lines[0]['id'].split('.')[1])
-    last_year = int(lines[-1]['id'].split('.')[1])
-    start_age = int(lines[0].find('td', attrs={'data-stat': 'age'}).text)
-
-    player.set_career_info(start_year, start_age, last_year, stats)
-
-
-def scrape_combine_table(table, player):
+def scrape_combine_table(soup, player):
     # TODO: Error handling
     # TODO: Get combine year
-    combine_data = table.tbody.tr
+    # Extract combine table from page
+    table = find_table(soup, "combine")
 
-    # Get combine results
-    dash = float(combine_data.find('td', attrs={'data-stat': 'forty_yd'}).text or 0.0)
-    bench = int(combine_data.find('td', attrs={'data-stat': 'bench_reps'}).text or 0)
-    broad = int(combine_data.find('td', attrs={'data-stat': 'broad_jump'}).text or 0)
-    shuttle = float(combine_data.find('td', attrs={'data-stat': 'shuttle'}).text or 0.0)
-    cone = float(combine_data.find('td', attrs={'data-stat': 'cone'}).text or 0.0)
-    vertical = float(combine_data.find('td', attrs={'data-stat': 'vertical'}).text or 0.0)
+    if table:
+        # Extract combine data from table
+        combine_data = table.tbody.tr
 
-    # If player infos are missing, get them from the combine table
-    height = int(combine_data.find('td', attrs={'data-stat': 'height'}).text or 0)
-    weight = int(combine_data.find('td', attrs={'data-stat': 'weight'}).text or 0)
-    pos = combine_data.find('td', attrs={'data-stat': 'pos'}).text
+        # Get combine results
+        dash = float(combine_data.find('td', attrs={'data-stat': 'forty_yd'}).text or 0.0)
+        bench = int(combine_data.find('td', attrs={'data-stat': 'bench_reps'}).text or 0)
+        broad = int(combine_data.find('td', attrs={'data-stat': 'broad_jump'}).text or 0)
+        shuttle = float(combine_data.find('td', attrs={'data-stat': 'shuttle'}).text or 0.0)
+        cone = float(combine_data.find('td', attrs={'data-stat': 'cone'}).text or 0.0)
+        vertical = float(combine_data.find('td', attrs={'data-stat': 'vertical'}).text or 0.0)
 
-    if player.height == 0: player.height = inches_to_cm(height)
-    if player.weight == 0: player.weight = lbs_to_kgs(weight)
-    if player.position == 'N/A': player.position = pos
-    if player.position_group == 'N/A': player.position_group = get_position_group(pos)
+        # If player infos are missing, get them from the combine table
+        height = int(combine_data.find('td', attrs={'data-stat': 'height'}).text or 0)
+        weight = int(combine_data.find('td', attrs={'data-stat': 'weight'}).text or 0)
+        pos = combine_data.find('td', attrs={'data-stat': 'pos'}).text
 
-    player.set_combine_results(dash, bench, broad, shuttle, cone, vertical)
+        if player.height == 0: player.height = inches_to_cm(height)
+        if player.weight == 0: player.weight = lbs_to_kgs(weight)
+        if player.position == 'N/A': player.position = pos
+        if player.position_group == 'N/A': player.position_group = get_position_group(pos)
+
+        player.set_combine_results(dash, bench, broad, shuttle, cone, vertical)
+
+    else:
+        logger.debug(f"[{player.pid}] - No combine table")
 
 
 def fetch_and_scrape_player_page(pid):
@@ -118,27 +143,12 @@ def scrape_player_page(page, pid):
     # Parse HTML file
     soup = BeautifulSoup(page, 'html.parser')
 
-    # Scrape the page
+    # Instanciate the player
     player = Player(pid)
 
-    header = soup.find('div', attrs={'id': 'meta'})
-    career_table = get_career_table(soup)
-    combine_table = find_table(soup, "combine")
-
-    if header is not None:
-        scrape_player_header(header, player)
-    else:
-        logger.debug(f"[{pid}] - No player header")
-
-    if career_table is not None:
-        logger.debug(f"[{pid}] - Career info found in {career_table['id']}")
-        scrape_career_table(career_table, player)
-    else:
-        logger.debug(f"[{pid}] - No career table")
-
-    if combine_table is not None:
-        scrape_combine_table(combine_table, player)
-    else:
-        logger.debug(f"[{pid}] - No combine table")
+    # Scrape the page
+    scrape_player_header(soup, player)
+    scrape_career_table(soup, player)
+    scrape_combine_table(soup, player)
 
     return player
